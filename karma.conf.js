0096b29b51ed9c1fa2fdd9dc6209f855d811fda9
JC Brand <jc@amboss.tech>
2026-05-07 20:26:46 +0200

    chore: apply prettier to package.json and karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,110 +1,105 @@
 // Karma configuration
 // Generated on Sat Jul 09 2022 11:32:08 GMT+0200 (Central European Summer Time)
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    config.set({
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: "",
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+        // frameworks to use
+        // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
+        frameworks: ["mocha"],
 
+        // list of files / patterns to load in the browser
+        files: [
+            "node_modules/mocha/mocha.css",
+            "node_modules/chai/chai.js",
 
-    // frameworks to use
-    // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
-    frameworks: ['mocha'],
+            "test/utils.js",
+            "test/testvectors.js",
+            "test/InMemorySignalProtocolStore.js",
 
+            {
+                pattern: "protos/WhisperTextProtocol.proto",
+                served: true,
+                type: "proto",
+                included: false,
+            },
+            { pattern: "protos/push.proto", served: true, type: "proto", included: false },
+            {
+                pattern: "build/curve25519_compiled.wasm",
+                served: true,
+                type: "wasm",
+                included: false,
+            },
+            "build/curve25519_concat.js",
+            "src/curve25519_worker_manager.js",
+            "build/components_concat.js",
 
-    // list of files / patterns to load in the browser
-    files: [
-      'node_modules/mocha/mocha.css',
-      'node_modules/chai/chai.js',
+            "src/Curve.js",
+            "src/crypto.js",
+            "src/helpers.js",
+            "src/KeyHelper.js",
+            "src/SignalProtocolAddress.js",
+            "src/SessionBuilder.js",
+            "src/SessionCipher.js",
+            "build/protobufs_concat.js",
+            "src/SessionRecord.js",
+            "src/SessionLock.js",
+            "src/NumericFingerprint.js",
 
-      'test/utils.js',
-      'test/testvectors.js',
-      'test/InMemorySignalProtocolStore.js',
+            "test/KeyHelperTest.js",
+            "test/NumericFingerprintTest.js",
+            "test/SessionBuilderTest.js",
+            "test/SessionCipherTest.js",
+            "test/SignalProtocolAddressTest.js",
+            "test/cryptoTest.js",
+            "test/helpersTest.js",
 
-      { pattern: 'protos/WhisperTextProtocol.proto', served: true, type: 'proto', included: false },
-      { pattern: 'protos/push.proto', served: true, type: 'proto', included: false },
-      { pattern: 'build/curve25519_compiled.wasm', served: true, type: 'wasm', included: false },
-      'build/curve25519_concat.js',
-      'src/curve25519_worker_manager.js',
-      'build/components_concat.js',
+            "test/SessionStore_test.js",
+            "test/SignedPreKeyStore_test.js",
+            "test/PreKeyStore_test.js",
+            "test/IdentityKeyStore_test.js",
+            "test/SignalProtocolStore_test.js",
+        ],
 
-      "src/Curve.js",
-      "src/crypto.js",
-      "src/helpers.js",
-      "src/KeyHelper.js",
-      "src/SignalProtocolAddress.js",
-      "src/SessionBuilder.js",
-      "src/SessionCipher.js",
-      "build/protobufs_concat.js",
-      "src/SessionRecord.js",
-      "src/SessionLock.js",
-      "src/NumericFingerprint.js",
+        // list of files / patterns to exclude
+        exclude: ["test/*~"],
 
-      'test/KeyHelperTest.js',
-      'test/NumericFingerprintTest.js',
-      'test/SessionBuilderTest.js',
-      'test/SessionCipherTest.js',
-      'test/SignalProtocolAddressTest.js',
-      'test/cryptoTest.js',
-      'test/helpersTest.js',
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
+        preprocessors: {},
 
-      'test/SessionStore_test.js',
-      'test/SignedPreKeyStore_test.js',
-      'test/PreKeyStore_test.js',
-      'test/IdentityKeyStore_test.js',
-      'test/SignalProtocolStore_test.js',
-    ],
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
+        reporters: ["progress"],
 
+        customDebugFile: "test/debug.html",
 
-    // list of files / patterns to exclude
-    exclude: [
-      'test/*~'
-    ],
+        // web server port
+        port: 9876,
 
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
-    preprocessors: {
-    },
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
-    reporters: ['progress'],
+        // start these browsers
+        // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
+        browsers: ["ChromeHeadless"],
 
-    customDebugFile: 'test/debug.html',
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
 
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
-    browsers: ['ChromeHeadless'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser instances should be started simultaneously
-    concurrency: Infinity
-  })
-}
+        // Concurrency level
+        // how many browser instances should be started simultaneously
+        concurrency: Infinity,
+    });
+};
